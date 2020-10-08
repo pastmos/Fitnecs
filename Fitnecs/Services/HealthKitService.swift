ad3b54816fc9a7edf0b6d72643cfc69d8a797ea9
Panov Sergey <passt@yandex.ru>
2020-10-08 18:26:55 +0300

    release 1.0.1(2)

--- Fitnecs/Services/HealthKitService.swift
+++ Fitnecs/Services/HealthKitService.swift
@@ -70,6 +70,42 @@ class HealthKitService {
     }
 
 
+    //Sleep
+    func getSleep(startDate: Date, endDate: Date, startOrder: Int, completion: @escaping (Double?, Error?)->()) {
+
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+
+                // Use a sortDescriptor to get the recent data first
+                let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+
+                // we create our query with a block completion to execute
+                let query = HKSampleQuery(sampleType: sleepType, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) { (query, tmpResult, error) -> Void in
+
+                    if error != nil {
+
+                        // something happened
+                        return
+
+                    }
+
+                    if let result = tmpResult {
+
+                        // do something with my data
+                        for item in result {
+                            if let sample = item as? HKCategorySample {
+                                let value = (sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue) ? "InBed" : "Asleep"
+                                print("Healthkit sleep: \(sample.startDate) \(sample.endDate) - value: \(value)")
+                            }
+                        }
+                    }
+                }
+
+                // finally, we execute our query
+            healthStore.execute(query)
+            }
+    }
+
+
     //Height
     func getHeight(completion: @escaping (Double?, Error?)->()) {
 
