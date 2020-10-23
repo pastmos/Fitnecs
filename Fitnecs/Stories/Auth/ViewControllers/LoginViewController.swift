7750da376663d9ad57d0044a5b2cd25a65c6201a
Panov Sergey <passt@yandex.ru>
2020-10-23 12:39:57 +0300

    Login layout done

--- Fitnecs/Stories/Auth/ViewControllers/LoginViewController.swift
+++ Fitnecs/Stories/Auth/ViewControllers/LoginViewController.swift
@@ -12,17 +12,30 @@ import SwiftMessages
 class LoginViewController: BaseViewController {
 
     // MARK: Properties
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var registerView: UIView!
+    @IBOutlet weak var headerView: UIView!
 
 
     var viewModel: LoginViewModelProtocol?
 
+    var keyboardHeight: CGFloat = 0
+    var difference: CGFloat = 0
+    var contentOffset: CGFloat = 0
 
     // MARK: Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        self.view.backgroundColor = Assets.Colors.fitnecsBaseColor.color
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil);
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil);
 
         viewModel?.updateState = { [weak self] state in
             DispatchQueue.main.async {
@@ -57,6 +70,26 @@ class LoginViewController: BaseViewController {
         viewModel?.openMainInterface()
     }
     
+    @IBAction func viewDidTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+
+    @objc func keyboardWillShow(sender: NSNotification) {
+        if let keyboardSize = (sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                keyboardHeight = keyboardSize.height
+            }
+        contentOffset = self.scrollView.contentOffset.y
+        difference = keyboardHeight - (view.bounds.height - (registerView.bounds.height + headerView.bounds.height) + self.scrollView.contentOffset.y)
+        if difference > 0 {
+            self.scrollView.contentOffset.y += difference
+        }
+
+
+    }
+
+    @objc func keyboardWillHide(sender: NSNotification) {
+        self.scrollView.contentOffset.y = contentOffset
+    }
 
     // MARK: Private
 
